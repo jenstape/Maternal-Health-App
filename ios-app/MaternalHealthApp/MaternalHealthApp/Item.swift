bb6aeac23563713f59b232d79913d29761262e1f
Jenelle <jms43@fordham.edu>
2026-07-14 16:53:41 -0400

    Initial commit: project structure setup

---
+++ ios-app/MaternalHealthApp/MaternalHealthApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MaternalHealthApp
+//
+//  Created by Jenelle Stapleton on 7/13/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
